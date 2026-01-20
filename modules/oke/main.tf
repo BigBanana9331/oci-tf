e11a2c9701e6e6e0ec6901f11f0d264d758f359c
Huy Nguyen <phantom39z1@gmail.com>
2026-01-20 23:49:24 +0700

    fix: node pool source details

--- modules/oke/main.tf
+++ modules/oke/main.tf
@@ -25,6 +25,24 @@ data "oci_core_subnets" "worker_subnet" {
   display_name   = var.worker_subnet_name
 }
 
+data "oci_containerengine_node_pool_option" "node_pool_option" {
+  node_pool_option_id = "all"
+}
+
+data "oci_core_images" "shape_specific_images" {
+  #Required
+  compartment_id = var.tenancy_ocid
+  shape          = "VM.Standard.E.Flex"
+}
+
+locals {
+  all_images          = data.oci_core_images.shape_specific_images.images
+  all_sources         = data.oci_containerengine_node_pool_option.node_pool_option.sources
+  compartment_images  = [for image in local.all_images : image.id if length(regexall("Oracle-Linux-[0-9]*.[0-9]*-20[0-9]*", image.display_name)) > 0]
+  oracle_linux_images = [for source in local.all_sources : source.image_id if length(regexall("Oracle-Linux-[0-9]*.[0-9]*-20[0-9]*", source.source_name)) > 0]
+  image_id            = tolist(setintersection(toset(local.compartment_images), toset(local.oracle_linux_images)))[0]
+}
+
 resource "oci_containerengine_cluster" "cluster" {
   name               = var.cluster_name
   compartment_id     = var.compartment_id
@@ -112,5 +130,10 @@ resource "oci_containerengine_node_pool" "node_pool" {
     node_pool_pod_network_option_details {
       cni_type = each.value.cni_type
     }
+  }
+
+  node_source_details {
+    image_id    = local.image_id
+    source_type = "IMAGE"
   }
 }
