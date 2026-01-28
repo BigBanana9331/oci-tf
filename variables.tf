f40791c2969ee5961c8eab374062a6ddab89c848
Huy Nguyen <phantom39z1@gmail.com>
2026-01-28 17:27:36 +0700

    feat: init schema

--- variables.tf
+++ variables.tf
@@ -10,15 +10,15 @@ variable "region" {
   type = string
 }
 
-variable "api_gateway" {
-  type = object({
-    compartment_id = string
-    vcn_name       = string
-    subnet_name    = string
-    gateway_name   = string
-    endpoint_type  = string
-    ip_mode        = string
-    nsg_names      = list(string)
-    tags           = object({ freeformTags = map(string), definedTags = map(string) })
-  })
-}
+# variable "api_gateway" {
+#   type = object({
+#     compartment_id = string
+#     vcn_name       = string
+#     subnet_name    = string
+#     gateway_name   = string
+#     endpoint_type  = string
+#     ip_mode        = string
+#     nsg_names      = list(string)
+#     tags           = object({ freeformTags = map(string), definedTags = map(string) })
+#   })
+# }
