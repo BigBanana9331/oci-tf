29537398aa02facb457d80ae4a4f6d5197e3d403
Huy Nguyen <phantom39z1@gmail.com>
2026-02-02 09:28:00 +0700

    feat: multi env

--- main.tf
+++ main.tf
@@ -11,35 +11,3 @@ module "vcn" {
   subnets        = each.value.subnets
   nsgs           = each.value.nsgs
 }
-
-# module "logging" {
-#   source         = "./modules/logging"
-#   app_name       = var.app_name
-#   environment    = var.environment
-#   compartment_id = var.compartment_ocid
-# }
-
-# module "bastion" {
-#   source         = "./modules/bastion"
-#   app_name       = var.app_name
-#   environment    = var.environment
-#   compartment_id = var.compartment_ocid
-#   depends_on     = [module.networking]
-# }
-
-# module "apigateway" {
-#   source         = "./modules/apigateway"
-#   app_name       = var.app_name
-#   environment    = var.environment
-#   compartment_id = var.compartment_ocid
-#   depends_on     = [module.networking]
-# }
-
-# module "container" {
-#   source         = "./modules/container"
-#   tenancy_ocid   = var.tenancy_ocid
-#   app_name       = var.app_name
-#   environment    = var.environment
-#   compartment_id = var.compartment_ocid
-#   depends_on     = [module.networking, module.logging]
-# }
