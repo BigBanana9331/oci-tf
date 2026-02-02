c6843ac6eaba62d50e514d2c8be63bb34b723e98
Nguyen Vo Bao Huy <54850943+BigBanana9331@users.noreply.github.com>
2026-02-02 13:10:14 +0700

    feat: add bastion

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-# module "bastion" {
-#   source                     = "./modules/bastion"
-#   compartment_id             = var.compartment_ocid
-#   environment                = var.environment
-#   app_name                   = var.app_name
-#   tags                       = local.tags
-#   vcn_name                   = var.bastion.vcn_name
-#   subnet_name                = var.bastion.subnet_name
-#   bastion_name               = var.bastion.bastion_name
-#   max_session_ttl_in_seconds = var.bastion.max_session_ttl_in_seconds
-# }
+module "bastion" {
+  source                     = "./modules/bastion"
+  compartment_id             = var.compartment_ocid
+  environment                = var.environment
+  app_name                   = var.app_name
+  tags                       = local.tags
+  vcn_name                   = var.bastion.vcn_name
+  subnet_name                = var.bastion.subnet_name
+  bastion_name               = var.bastion.bastion_name
+  max_session_ttl_in_seconds = var.bastion.max_session_ttl_in_seconds
+}
